999c8b5a513e9010e5980cd272334f00fceb4e10
Ben Rudolph <226749@shakopeeschools.org>
2021-11-10 09:34:53 -0600

    edits

--- Version6.0/user24/js/myscript.js
+++ Version6.0/user24/js/myscript.js
@@ -1,2 +1,23 @@
 let d = new Date();
 document.body.innerHTML = "<h1>Time right now is:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()</h1>"
+
+
+
+
+
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Time right now is: </title>
+
+</head>
+
+<body>
+<script>
+let d = new Date();
+document.body.innerHTML = "<h1>Time right now is:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()
+"</h1>"
+
+</script>
+</body>
